f2d51e1ab8f7ceaced7545901647226776644847
Brad Chang <cutebrady@gmail.com>
2018-05-23 09:47:28 +0800

    Show tensorflow version with python 2/3

---
+++ tf_version.sh
@@ -0,0 +1,2 @@
+python -c 'import sys;import tensorflow as tf; print("Python %s, TF:%s" % (sys.version.split()[0], tf.__version__))'
+python3 -c 'import sys;import tensorflow as tf; print("Python %s, TF:%s" % (sys.version.split()[0], tf.__version__))'
